22671f25014049042776f22acfc809fd168c090b
IPHSCHOIKYU <103420433+IPHSCHOIKYU@users.noreply.github.com>
2022-05-09 16:25:09 +0900

    Add files via upload

---
+++ ForExample/src/ContinueTest.java
@@ -0,0 +1,16 @@
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		// continue문 = 조건식으로 이동
+		for(int i=1; i<=10; i++) {
+			if(i%2 != 0) {
+				continue;
+			}	
+			System.out.println(i);
+			
+		}
+
+	}
+
+}
---
+++ ForExample/src/WhileExample01.java
@@ -0,0 +1,14 @@
+
+public class WhileExample01 {
+
+	public static void main(String[] args) {
+		// 반복문 - while
+		int 안승현은_멍청이인가 = 1;
+		while(안승현은_멍청이인가<=10) {
+			System.out.println(안승현은_멍청이인가);
+			안승현은_멍청이인가++;
+		}
+
+	}
+
+}
